f1ceeaa721fb2ab350bc2cc503959b9639f13cbd
rodkamp <rodkamp@yahoo.com>
2017-08-27 12:05:16 -0400

    first commit

---
+++ src/main/java/com/gang/learn/LearnSLF4J/App.java
@@ -0,0 +1,21 @@
+package com.gang.learn.LearnSLF4J;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		
+		Logger logger = LoggerFactory.getLogger(App.class);
+		logger.trace("msg trace");
+		logger.debug("msg degug");
+		logger.info("msg info");
+		logger.warn("msg warn");
+		logger.error("msg error");
+		
+	}
+}
